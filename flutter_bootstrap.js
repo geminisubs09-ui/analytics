0f0758950598438f7021315785031be80ebda36b
Deploy <deploy@example.com>
2026-06-14 18:01:29 +0545

    deploy: update web app with Nepali formatting, dynamic group sales, 30-day trends, and custom date range picker

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "688248928" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "492733539" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
